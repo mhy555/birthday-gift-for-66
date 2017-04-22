7d8c8beea805d28238d967307d17068233791782
hanyue ma <hanyuema@hanyuetekiMacBook-Air.local>
2017-04-22 17:15:28 +0800

    add basic dependencies

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="home">
+    <div class="slot main">
+      <div class="text">
+        Happy Birthday!!
+      </div>
+      <div class="date">
+        2017.6.7
+      </div>
+    </div>
+    <div class="slot memory">
+      
+    </div>
+    <div class="slot photo">
 
+    </div>
+    <div class="mail-container">
+      <div class="write-mail">
+
+      </div>
+      <div class="mail-box">
+
+      </div>
+    </div>
   </div>
 </template>
 
